ea30ae59dff6070224e264c4b312369ce178ccfb
Thiago Britto Borges <tbrittoborges@dundee.ac.uk>
2019-03-14 11:47:38 +0100

    Start script with general tools

---
+++ scripts/utils.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+"""
+Created on 11:13 2019-03-14 2019 
+
+"""
+from pathlib import Path
+
+def basename(path, suffix=None):
+    if suffix:
+        return str(Path(path).with_suffix(suffix).name)
+    return str(Path(path).name)
