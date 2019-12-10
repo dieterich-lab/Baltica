fca977c09ca8caeebc1d50cc2b4a5b9aa71d5d4f
tbrittoborges <Thiago.BrittoBorges@uni-heidelberg.de>
2019-12-10 17:26:50 +0100

    add script to filter non-coding transcripts

---
+++ analysis/filter_noncoding_gtf.R
@@ -0,0 +1,11 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: thiago
+# Created on: 10.12.19
+
+library(rtracklayer)
+gtf <- import('/biodb/genomes/mus_musculus/GRCm38_87/GRCm38.87.gtf')
+gtf_coding <- subset(gtf, transcript_biotype ==  'protein_coding')
+export(gtf_coding, 'GRCm38.87_coding.gtf')
+
+
