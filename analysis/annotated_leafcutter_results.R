895abe14cfb64ca377da949ec7bad1c730aa3b76
tbrittoborges <Thiago.BrittoBorges@uni-heidelberg.de>
2019-12-17 16:03:11 +0100

    add canonical and rank.psi columns

--- analysis/annotated_leafcutter_results.R
+++ analysis/annotated_leafcutter_results.R
@@ -16,14 +16,14 @@ option_list = list(
     type = "character",
     default = NULL,
     help = "Leafcutter result parsed",
-    metavar = "character",
+    metavar = "character"
   ),
   make_option(
     c("-a", "--annotation"),
     type = "character",
     default = NULL,
     help = "Annotation in the GTF/GFF format",
-    metavar = "character",
+    metavar = "character"
   )
 )
 
@@ -35,6 +35,7 @@ if (!file.exists(opt$input)) {
 } else if (!file.exists(opt$annotation)) {
   stop("Annotation not found.", call.=FALSE)
 }
+
 
 df <-  read_csv(
   opt$input,
@@ -50,42 +51,55 @@ df <-  read_csv(
   )
 )
 
+canonical.usage.rank <- df %>%
+  group_by(contrast, cluster) %>%
+  group_modify(~ broom::tidy(rank(-.x[[5]] )))
+df['canonical.usage.rank'] <- canonical.usage.rank$x
 # TODO pval and deltapsi as options
 
 df <- df %>%
   filter(p.adjust < 0.05,  abs(deltapsi) > .1)
 
-
 df[!df$strand %in% c('+', '-'), "strand"] <- '*'
 
 gtf <- rtracklayer::import.gff(opt$annotation)
-genes <- gtf[gtf$type == "gene"]
+multi.exon.genes <- mcols(gtf) %>%
+  as.data.frame() %>%
+  group_by(gene_id) %>%
+  summarise(n_exons = n_distinct(exon_id)) %>%
+  filter(n_exons >= 1) %>%
+  pull(gene_id)
+
+genes <- gtf[gtf$type == "gene" & gtf$gene_id %in% multi.exon.genes]
 genes.list <- split(genes, genes$gene_id)
 
-# find introns
-# exons <- subset(gtf, type == "exon")
-# exons.list <- GenomicRanges::split(exons, exons$gene_id)
-# introns.list <- lapply(exons.list, gaps)
-# introns <- unlist(as(introns.list, "GRangesList"))
-# introns <- subsetByOverlaps(introns, genes, type='within')
-
-# # ...
-# hits <- findOverlaps(gr, introns)
-# overlaps <- pintersect(gr[queryHits(hits)], introns[subjectHits(hits)])
-# percentOverlap <- width(overlaps) / width(introns[subjectHits(hits)])
-# # tst <- split(subjectHits(hits), queryHits(hits))
-# gr['annotated'] <- percentOverlap == 1 # TODO ?
-# # gr['canonical'] <- PSI ref column >  percentOverlap == 1 # TODO ?
-
+# Process annotated introns
+exons <- subset(gtf, type == "exon")
+exons.list <- GenomicRanges::split(exons, exons$gene_id)
+introns.list <- lapply(exons.list, gaps)
+introns <- unlist(as(introns.list, "GRangesList"))
+introns <- subsetByOverlaps(introns, genes, type='within')
 
 gr <- GRanges(df[, c("chr", "start", "end", "strand")])
+# Find annotated introns
+hits <- findOverlaps(gr, introns, select = 'arbitrary')
+overlaps <- pintersect(
+  gr[complete.cases(hits)],
+  introns[hits[complete.cases(hits)]])
+percentOverlap <- width(overlaps) / width(introns[hits[complete.cases(hits)]])
+
+df['annotated'] <- FALSE
+df[complete.cases(hits), 'annotated'] <- if_else(
+  percentOverlap == 1, TRUE, FALSE)
+
+# Process events overlaps
 gr_on_gtf <- mergeByOverlaps(gr, genes)
 
 df$junction <- as.character(str_glue_data(df, "{chr}:{start}-{end}:{strand}"))
 gr_on_gtf$gr <- as.character(gr_on_gtf$gr)
 gr_on_gtf <- as.data.frame(gr_on_gtf)
-gr_on_gtf <-
-  dplyr::select(gr_on_gtf, c("gr", "gene_id", "gene_name", "gene_biotype"))
+gr_on_gtf <- dplyr::select(gr_on_gtf,
+                           c("gr", "gene_id", "gene_name", "gene_biotype"))
 df <- left_join(df, gr_on_gtf, by = c("junction" = "gr"))
 df <- distinct(df)
 
@@ -105,6 +119,8 @@ df$up <- tidyr::replace_na(df$up, 0)
 df$down <- tidyr::replace_na(df$down, 0)
 df["overlaps"] <- df$up - df$down
 
+#
+
 out.path <- dirname(opt$input)
 
 message('Writting the annotated output to ', file.path(out.path, 'leafcutter_junctions_annotated.csv'))
@@ -121,30 +137,30 @@ for (cont in unique(df$contrast)) {
 }
 
 col_desc <- read.csv(
-    text = "contrast,pair-wise comparison
-    intron,intron identifier (refer to this contrast)
-    logef,likelihood ratio: null model vs alt model
-    deltapsi,PSIref - PSIalt
-    chr,chromosome or contig
-    start,genomic start position
-    end,genomic end position
-    strand,RNA strand
-    cluster,unique identifier for the cluster (refer to this contrast)
-    status,OK or reason not to tested
-    loglr,log effect size
-    df,degrees of freedom (number of introns within a cluster - 1)
-    p,p-value
-    p.adjust,adjusted p-value (method = FDR)
-    genes,gene name
-    junction,junction on the format chr:start-end:strand
-    gene_id,gene identifier from Ensembl
-    gene_name,gene symbol
-    gene_biotype,gene biotype from Ensembl
-    up,junction upregulated `up` times (all contrasts)
-    down,junction downregulated `down` times (all contrasts)
-    overlaps,up - down
-    columns with condition name,PSI value (fitted junction usage)"
-  )
+  text = "contrast,pair-wise comparison
+  intron,intron identifier (refer to this contrast)
+  logef,likelihood ratio: null model vs alt model
+  deltapsi,PSIref - PSIalt
+  chr,chromosome or contig
+  start,genomic start position
+  end,genomic end position
+  strand,RNA strand
+  cluster,unique identifier for the cluster (refer to this contrast)
+  status,OK or reason not to tested
+  loglr,log effect size
+  df,degrees of freedom (number of introns within a cluster - 1)
+  p,p-value
+  p.adjust,adjusted p-value (method = FDR)
+  genes,gene name
+  junction,junction on the format chr:start-end:strand
+  gene_id,gene identifier from Ensembl
+  gene_name,gene symbol
+  gene_biotype,gene biotype from Ensembl
+  up,junction upregulated `up` times (all contrasts)
+  down,junction downregulated `down` times (all contrasts)
+  overlaps,up - down
+  columns with condition name,PSI value (fitted junction usage)"
+)
 
 
 addWorksheet(wb,  'col_desc')
