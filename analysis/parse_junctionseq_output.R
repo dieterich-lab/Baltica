da9a1118cdcb0ca1033e07443caf4a59c95f0951
tbrittoborges <Thiago.BrittoBorges@uni-heidelberg.de>
2020-02-04 14:37:51 +0100

    cleanup

--- analysis/parse_junctionseq_output.R
+++ analysis/parse_junctionseq_output.R
@@ -4,11 +4,13 @@ suppressPackageStartupMessages({
   library(stringr)
   library(readr)
   library(dplyr)
+  library(optparse)
   library(tidylog)
+
 })
 
 
-option_list = list(
+option_list <- list(
   make_option(
     c("-i", "--input"),
     type = "character",
@@ -25,35 +27,45 @@ option_list = list(
   )
 )
 
-opt <- parse_args(OptionParser(option_list=option_list))
+opt <- parse_args(OptionParser(option_list = option_list))
 
-# JunctionSeq outputs is one junction per line format
-res <- read_table2(opt$input, col_types = cols(
-  .default = col_double(),
-  chr = col_character(),
-  start = col_integer(),
-  end = col_integer(),
-  featureID = col_character(),
-  geneID = col_character(),
-  countbinID = col_character(),
-  testable = col_logical(),
-  status = col_character(),
-  allZero = col_logical(),
-  strand = col_character(),
-  transcripts = col_character(),
-  featureType = col_character()
-))
+message("Loading JunctionSeq result")
+res <- read_table2(
+  opt$input,
+  col_types = cols(
+    .default = col_double(),
+    chr = col_character(),
+    start = col_integer(),
+    end = col_integer(),
+    featureID = col_character(),
+    geneID = col_character(),
+    countbinID = col_character(),
+    testable = col_logical(),
+    status = col_character(),
+    allZero = col_logical(),
+    strand = col_character(),
+    transcripts = col_character(),
+    featureType = col_character()
+  )
+)
+
+message("Loading processing the table")
 
 res <- res %>%
-  filter(padjust < opt$pval_cuttoff, status == 'OK') %>%
-  # filter_at(vars(contains('log2')), any_vars(abs(.) <= opt$foldchange_cutoff)) %>%
-  select(chr, start, end, strand, padjust, contains('log2FC('), geneID, featureType) %>%
-  pivot_longer(contains("log2FC("), names_to = "comparison", values_to = "l2fc") %>%
-  filter(abs(l2fc) > opt$foldchange_cutoff)
+  select(chr,
+         start,
+         end,
+         strand,
+         padjust,
+         contains('log2FC('),
+         geneID,
+         featureType)  %>%
+  pivot_longer(contains("log2FC("),
+               names_to = "comparison",
+               values_to = "l2fc")
+  # filter(abs(l2fc) > opt$foldchange_cutoff)
 
-comp_name <- res$comparison %>% str_match('log2FC\\((.*)\\)' )
-res$comparison <- comp_name[,2 ] %>% str_replace('/', '_vs_')
+comp_name <- res$comparison %>% str_match('log2FC\\((.*)\\)')
+res$comparison <- comp_name[, 2] %>% str_replace('/', '_vs_')
 
 write_csv(res, opt$output)
-
-
