2c45511ad19ccfbd95650143b9da1ef3477cc6dd
tbrittoborges <thiago.brittoborges@uni-heidelberg.de>
2021-06-23 14:44:00 +0200

    move CLI framework from argparse to click to support snakemake cli

--- baltica/command.py
+++ baltica/command.py
@@ -1,164 +1,125 @@
 #!/usr/bin/env python
 """
-Command line interface driver for Baltica
+Command line interface for Baltica
 
-Modified from https://github.com/charlesreid1/2019-snakemake-cli
 """
 import os
-import argparse
 from pathlib import Path
 import sys
 import yaml
+import logging
+import subprocess
 
 import snakemake
-
-try:
-    import baltica
-    baltica_installed = True
-except ImportError:
-    baltica_installed = False
+import click
 
 _program = "baltica"
 __version__ = "1.0.1"
-p = Path(__file__)
 
-desc = f"{_program} implements workflows for differential junction usage and consequence analysis. Visit " \
-       f"https://github.com/dieterich-lab/Baltica for more information. "
+baltica_path = Path(__file__)
 
 
-def main():
-    parser = argparse.ArgumentParser(prog=_program,
-                                     description=desc,
-                                     usage=f"{_program} <workflow> <config> <options>")
-    parser.add_argument(
-        "workflow",
-        choices=["rmats", "leafcutter", "majiq", "junctionseq", "qc", "stringtie", "analysis"],
-        help="Workflow to be run"
-    )
-    parser.add_argument(
-        "config",
-        type=Path,
-        help="Configuration file for the workflow."
-    )
-    parser.add_argument(
-        "-n",
-        "--dry-run",
-        action="store_true",
-        help="Execute a test run and output commands to be run."
-    )
-    parser.add_argument(
-        "-f",
-        "--force",
-        action="store_true",
-        help="Force the workflow to run despite the presence of an output."
-    )
-    parser.add_argument(
-        "--use-conda",
-        action="store_true",
-        help="Use conda to install the requirements."
-    )
-    # TODO add slurm profile
-    # parser.add_argument(
-    #     "--profile",
-    #     default="slurm",
-    #     help="Snakemake cluster profile used to run the workflows"
-    # ),
-    parser.add_argument(
-        "--use-envmodule",
-        action="store_true",
-        help="Use environment modules for requirements."
-    )
-    parser.add_argument(
-        "--unlock",
-        action="store_true",
-        help="Use directory from previously running DAG that was interrupted."
-    ),
-    parser.add_argument(
-        "--cluster",
-        metavar='str',
-        default="sbatch --parsable --mem {cluster.mem} --out {cluster.out} --error {cluster.out} -c {cluster.cpu} --partition {cluster.partition}",
-        help='Snakemake cluster parameter (default: %(default)s)'
-    ),
-    parser.add_argument(
-        "--cluster-config",
-        metavar='str',
-        default=str(p.parent / 'cluster.yml'),
-        help='Snakemake cluster configuration (default: %(default)s)'
-    ),
-    parser.add_argument(
-        "--nodes",
-        type=int,
-        default=10
-    )
-    parser.add_argument(
-        "--version",
-        action="version",
-        version="%(prog)s " + __version__
-    )
+# from https://stackoverflow.com/a/56944256/1694714
+class CustomFormatter(logging.Formatter):
+    """Logging Formatter to add colors and count warning / errors"""
+
+    grey = "\x1b[38;21m"
+    yellow = "\x1b[33;21m"
+    green = "\x1b[32;21m"
+    red = "\x1b[31;21m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "baltica:\t| %(message)s"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: green + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+def avaiable_workflows(baltica_path):
+    smk_files = baltica_path.parent.glob('*.smk')
+    return [x.with_suffix('').name for x in smk_files]
+
+avaiable_workflows_ = avaiable_workflows(baltica_path)
+
+logger = logging.getLogger(__file__)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(CustomFormatter())
+logger.addHandler(ch)
+
+# https://click.palletsprojects.com/en/8.0.x/advanced/#forwarding-unknown-options
+# unknown options are passed to snakemake
+@click.command(context_settings=dict(ignore_unknown_options=True))
+@click.argument("workflow", type=click.Choice(avaiable_workflows_, case_sensitive=False), default='baltica')
+@click.argument("config",  type=click.Path(exists=True))
+@click.option('-v', '--verbose', is_flag=True, help='Enables verbose mode.')  
+@click.argument('snakemake_args', nargs=-1, type=click.UNPROCESSED) 
+def baltica(workflow, config, verbose, snakemake_args):
+    f"""{_program} {__version__}implements workflows for differential junction
+     usage methods, and method integration and analysis. Visit
+      https://github.com/dieterich-lab/Baltica for more information. 
+      
+      Runs baltica WORKFLOW with CONFIG and SNAKEMAKE_ARGS"""
+    # TODO add link to baltica docs with important snakemake parameters
+    if verbose:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.ERROR)
 
     # Error if older version of snakemake is installed
     min_snakemake_version = "5.2"
     try:
         snakemake.utils.min_version(min_snakemake_version)
     except snakemake.exceptions.WorkflowError as e:
-        print(f'{_program} requires Snakemake version >= {min_snakemake_version}:', file=sys.stderr)
-        print(e, file=sys.stderr)
+        logger.error(
+            f'{_program} requires Snakemake version >= {min_snakemake_version}:',  
+            exc_info=True)
         sys.exit(1)
-
-    argv = parser.parse_args(tuple(sys.argv[1:]))
     # check if workflow file is readable
-    snakefile = (p.parent / argv.workflow).with_suffix(".smk")
+    snakefile = (baltica_path.parent / workflow).with_suffix(".smk")
 
-    with open(argv.config) as fin:
+    with open(config) as fin:
         workflow_info = yaml.safe_load(fin)
 
     target = workflow_info["path"]
-
-    if not snakefile.exists:
-        print(f"Error: cannot find Snakefile for the {argv.workflow} workflow: {snakefile} \n", file=sys.stderr)
-        sys.exit(1)
-
-    if not argv.config.exists:
-        print(f"Error: cannot find configuration file {argv.config} \n", file=sys.stderr)
-        sys.exit(1)
-
-    # TODO handling cluster profile profile_config = Path(snakemake.get_profile_file(argv.profile, 'config.yaml',
-    #  return_default=True)) SBATCH_DEFAULTS = """'sbatch -p {cluster.partition} --mem {cluster.mem} --out {
-    #  cluster.out} --error {cluster.out} -c {cluster.cpu}'""" CLUSTER_CONFIG = "cluster.yaml"
-
-    print("--------")
-    print("details:")
-    print("\tsnakefile: {}".format(snakefile))
-    print("\tconfiguration file: {}".format(argv.config))
-    print("\twork directory: {}".format(target))
-    print("--------")
-
+    
     try:
         os.makedirs(Path(target) / 'logs/')
     except FileExistsError:
         pass
 
-    # run Baltica workflow
-    success = snakemake.snakemake(
-        snakefile,
-        configfiles=[argv.config],
-        workdir=str(target),
-        printshellcmds=True,
-        dryrun=argv.dry_run,
-        forceall=argv.force,
-        use_conda=argv.use_conda,
-        use_env_modules=argv.use_envmodule,
-        cluster=argv.cluster,
-        cluster_config=argv.cluster_config,
-        cluster_sync=None,
-        unlock=argv.unlock,
-        nodes=argv.nodes
-    )
+    snakemake_args = list(snakemake_args)    
+    if verbose:
+        snakemake_args.extend(['--printshellcmds', '--verbose', '--reason'])
+    if not any([x in snakemake_args for x in ['--cores', '-c', '--job', '-j']]):
+        snakemake_args.append('-j1')
+    
+    logger.info(
+        f"""Starting baltica (v{__version__}) analysis with:
+    WORKFLOW: {workflow} (from {snakefile})
+    CONFIGURATION: {config}
+    TARGET DIRECTORY: {target}   
+    SNAKEMAKE ARGUMENTS: {''.join(snakemake_args)}
+    """)
 
-    if success:
-        return 0
-    return 1
+    cmd = [
+        'snakemake',
+        '--configfile', config,
+        '--snakefile', str(snakefile),
+        *snakemake_args]
 
+    logger.debug('Start of snakemake logger:')
+    result = subprocess.run(cmd)
+    return result.check_returncode
 
 if __name__ == "__main__":
-    main()
+    baltica()
