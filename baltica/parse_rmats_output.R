4728c953f84e9375f82182eec06c75647a74312d
tbrittoborges <thiago.brittoborges@uni-heidelberg.de>
2021-07-20 18:16:41 +0200

    fallback

--- baltica/parse_rmats_output.R
+++ baltica/parse_rmats_output.R
@@ -162,7 +162,6 @@ res <- lapply(setNames(names(res), names(res)), function(x) {
 })
 res <- as(res, "GRangesList")
 res <- unlist(res)
-seqnames(res) <- gsub(x = seqnames(res), pattern = "^chr", replacement = "")
 
 message("Number of junctions after filtering ", length(res))
 write_csv(data.frame(res), opt$output)
