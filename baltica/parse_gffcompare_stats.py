d69003f8cfb7d7901f71416fda8dc54374098f24
tbrittoborges <thiago.brittoborges@uni-heidelberg.de>
2022-05-10 16:49:07 +0200

    lint parse_gffcompare_stats

--- baltica/parse_gffcompare_stats.py
+++ baltica/parse_gffcompare_stats.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys
+import pkgutil
 from ttp import ttp
-
 
 try:
     if 'snakemake' in globals():
@@ -18,35 +18,7 @@ except (TypeError, IndexError):
         f'Please use the script as: \n    {sys.argv[0]} input_ output \n or use it within snakemake')
     sys.exit(1)
 
-template = """
-<group name="{{ file_name }}">
-
-<group name="header">
-#= Summary for dataset: {{ file_name }} 
-#     Query mRNAs :      {{ query_mrna }} in      {{ query_loci }} loci  ({{ query_multiexon }} multi-exon transcripts)
-#            ({{ query_muti_transcript_loci }} multi-transcript loci, {{ transcripts_per_locus }} transcripts per locus)
-# Reference mRNAs :      {{ ref_mrna }} in      {{ ref_loci }} loci  ({{ ref_mutiexon }} multi-exon)
-# Super-loci w/ reference transcripts:       {{ super_loci }}
-
-</group>
-<group name="table">
-{{ ignore("\s+") }}{{ level | _start_ }} level:    {{ sensitivity }}     |    {{ precision }}    |{{ _end_ }}
-</group>
-
-<group name="matching">
-{{ ignore("\s+") }}Matching {{level | ORPHRASE}}:      {{number}}\n
-</group>
-
-<group name="missed">
-{{ ignore("\s+") }}Missed {{level}}:{{ ignore("\s+") }}{{ num }}/{{ den }}\t({{ ignore("\s+") }}{{ perc }}%)
-</group>
-
-<group name="novel">
-{{ ignore("\s+") }}Novel {{level}}:{{ ignore("\s+") }}{{ num }}/{{ den }}\t({{ ignore("\s+") }}{{ perc }}%)
-</group>
-
-</group>
-"""
+template = pkgutil.get_data(__name__, "resources/gff_compare_template.txt")
 
 parser = ttp(data=data, template=template)
 parser.parse()
